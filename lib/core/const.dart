c841a5eca889b8a768c63d2c951ee2895ee893f7
SOUHAILBENBRIK <benbriksouhail43@gmail.com>
2023-10-14 20:30:50 +0100

    first commit

---
+++ lib/core/const.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class AppConst {
+  static final cites = [
+    {"label": "Tunis", "value": "Tunis"},
+    {"label": "Sousse", "value": "Sousse"},
+    {"label": "Ariana", "value": "Ariana"},
+    {"label": "Ben Arous", "value": "Ben Arous"},
+    {"label": "Manouba", "value": "Manouba"},
+    {"label": "Nabeul", "value": "Nabeul"},
+    {"label": "Zaghouan", "value": "Zaghouan"},
+    {"label": "Bizerte", "value": "Bizerte"},
+    {"label": "Béja", "value": "Béja"},
+    {"label": "Jendouba", "value": "Jendouba"},
+    {"label": "Kef", "value": "Kef"},
+    {"label": "Siliana", "value": "Siliana"},
+    {"label": "Sidi Bouzid", "value": "Sidi Bouzid"},
+    {"label": "Kairouan", "value": "Kairouan"},
+    {"label": "Kasserine", "value": "Kasserine"},
+    {"label": "Sfax", "value": "Sfax"},
+    {"label": "Gabes", "value": "Gabes"},
+    {"label": "Medenine", "value": "Medenine"},
+    {"label": "Gafsa", "value": "Gafsa"},
+    {"label": "Tozeur", "value": "Tozeur"},
+    {"label": "Kebili", "value": "Kebili"},
+    {"label": "Tataouine", "value": "Tataouine"},
+    {"label": "Mahdia", "value": "Mahdia"},
+    {"label": "Sidi Bou Said", "value": "Sidi Bou Said"}
+  ];
+  static final mainColor = Color.fromRGBO(0, 128, 128, 1);
+}
---
+++ lib/core/core.dart
@@ -0,0 +1,13 @@
+/*
+We believe, the class name must be unique. 
+If there is a conflicting class name in this file,
+it means you have to rename it to something more unique.
+*/
+export 'package:train_ticket_buying_app/booking_detail/controller/booking_detail_controller.dart';
+export 'package:train_ticket_buying_app/booking_detail/view/booking_detail_view.dart';
+export 'package:train_ticket_buying_app/dashboard/controller/dashboard_controller.dart';
+export 'package:train_ticket_buying_app/dashboard/view/dashboard_view.dart';
+export 'package:train_ticket_buying_app/main_navigation/controller/main_navigation_controller.dart';
+export 'package:train_ticket_buying_app/main_navigation/view/main_navigation_view.dart';
+export 'package:train_ticket_buying_app/seat_picker/controller/seat_picker_controller.dart';
+export 'package:train_ticket_buying_app/seat_picker/view/seat_picker_view.dart';
